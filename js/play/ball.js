dab1ed0621d0e2c2fa37ec41ee737dd0f9a498d6
Alexander Litty <github@alexlitty.com>
2016-06-18 16:30:23 -0700

    Added simple physics to the ball. Bounces off top and bottom walls.

--- js/play/ball.js
+++ js/play/ball.js
@@ -28,10 +28,58 @@ function Ball() {
 
 /**
  * Move the ball according "physics."
+ *
+ * Unless provided with a velocity, the ball will move according to its internal velocity.
  */
-Ball.prototype.move = function() {
+Ball.prototype.move = function(velocity) { 
 
-    this.graphic.style.left = addPixels(this.graphic.style.left, this.velocity.x);
-    this.graphic.style.top = addPixels(this.graphic.style.top, this.velocity.y);
+    // Use default, internal velocity.
+    if (!velocity) {
+        velocity = this.velocity;
+    }
 
+    // Get current position.
+    var bounds = this.graphic.getBoundingClientRect();
+
+    // Calculate target position.
+    var targetBounds = {
+        left: parseInt(this.graphic.style.left) + velocity.x,
+        top: parseInt(this.graphic.style.top) + velocity.y
+    };
+
+    targetBounds.right = targetBounds.left + bounds.width;
+    targetBounds.bottom = targetBounds.top + bounds.height;
+
+    // Collision with top wall.
+    if (targetBounds.top <= 0) {
+        this.bounceVertically();
+    }
+
+    // Collision with bottom wall.
+    if (targetBounds.bottom >= document.documentElement.clientHeight) {
+        this.bounceVertically();
+    }
+
+    // Move the ball to the new position.
+    this.graphic.style.left = targetBounds.left + "px";
+    this.graphic.style.top = targetBounds.top + "px";
+
+}
+
+
+/**
+ * Perform a vertical bounce.
+ */
+Ball.prototype.bounceVertically = function() {
+    console.log("Bouncing vertically");
+    this.velocity.y = -this.velocity.y;
+}
+
+
+/**
+ * Perform a horizontal bounce.
+ */
+Ball.prototype.bounceHorizontally = function() {
+    console.log("Bouncing horizontally");
+    this.velocity.x = -this.velocity.x;
 }
